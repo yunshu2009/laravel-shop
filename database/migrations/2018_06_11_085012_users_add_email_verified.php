96fe9e446d1663f8fdf272fff8fd45cfd727c6b3
yunshu <yunshu_luo@163.com>
2018-08-25 22:24:08 +0800

    Update 2018_06_11_085012_users_add_email_verified.php

--- database/migrations/2018_06_11_085012_users_add_email_verified.php
+++ database/migrations/2018_06_11_085012_users_add_email_verified.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/*
+创建迁移：php artisan make:migration users_add_email_verified --table=users
+*/
 class UsersAddEmailVerified extends Migration
 {
     public function up()
