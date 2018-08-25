41bbcd92259a4299001299871c0781093a4ffdca
yunshu <yunshu_luo@163.com>
2018-08-25 22:31:37 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,8 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /*$casts 属性提供了一个便利的方法来将数据库字段值转换为常见的数据类型，$casts 属性应是一个数组，且数组的键是那些需要被转换的字段名，值则是你希望转换的数据类型。
+    支持转换的数据类型有： integer，real，float，double，string，boolean，object，array，collection，date，datetime 和 timestamp。*/
     protected $casts = [
         'email_verified' => 'boolean',
     ];
